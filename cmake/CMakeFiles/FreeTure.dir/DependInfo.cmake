be53d69a78f3489855e0314d63ddd8434b2ecdc3
Mattia Malacarne <mattia.malacarne@n-3.it>
2023-04-06 12:35:49 +0200

    Old cmake with new Find

---
+++ cmake/CMakeFiles/FreeTure.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nova/Repository/freeture/src/AcqSchedule.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/AcqSchedule.cpp.o"
+  "/home/nova/Repository/freeture/src/AcqThread.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/AcqThread.cpp.o"
+  "/home/nova/Repository/freeture/src/Base64.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Base64.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraFrames.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraFrames.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraGigeAravis.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraGigeAravis.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraGigePylon.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraGigePylon.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraGigeTis.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraGigeTis.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraV4l2.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraV4l2.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraVideo.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraVideo.cpp.o"
+  "/home/nova/Repository/freeture/src/CameraWindows.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CameraWindows.cpp.o"
+  "/home/nova/Repository/freeture/src/CfgLoader.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CfgLoader.cpp.o"
+  "/home/nova/Repository/freeture/src/CfgParam.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/CfgParam.cpp.o"
+  "/home/nova/Repository/freeture/src/Conversion.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Conversion.cpp.o"
+  "/home/nova/Repository/freeture/src/DetThread.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/DetThread.cpp.o"
+  "/home/nova/Repository/freeture/src/DetectionTemplate.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/DetectionTemplate.cpp.o"
+  "/home/nova/Repository/freeture/src/DetectionTemporal.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/DetectionTemporal.cpp.o"
+  "/home/nova/Repository/freeture/src/Device.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Device.cpp.o"
+  "/home/nova/Repository/freeture/src/EParser.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/EParser.cpp.o"
+  "/home/nova/Repository/freeture/src/Ephemeris.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Ephemeris.cpp.o"
+  "/home/nova/Repository/freeture/src/ErrorManager.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/ErrorManager.cpp.o"
+  "/home/nova/Repository/freeture/src/ExposureControl.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/ExposureControl.cpp.o"
+  "/home/nova/Repository/freeture/src/Fits.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Fits.cpp.o"
+  "/home/nova/Repository/freeture/src/Fits2D.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Fits2D.cpp.o"
+  "/home/nova/Repository/freeture/src/Fits3D.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Fits3D.cpp.o"
+  "/home/nova/Repository/freeture/src/Frame.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Frame.cpp.o"
+  "/home/nova/Repository/freeture/src/GlobalEvent.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/GlobalEvent.cpp.o"
+  "/home/nova/Repository/freeture/src/Histogram.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Histogram.cpp.o"
+  "/home/nova/Repository/freeture/src/HistogramGray.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/HistogramGray.cpp.o"
+  "/home/nova/Repository/freeture/src/HistogramRGB.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/HistogramRGB.cpp.o"
+  "/home/nova/Repository/freeture/src/ImgProcessing.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/ImgProcessing.cpp.o"
+  "/home/nova/Repository/freeture/src/LocalEvent.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/LocalEvent.cpp.o"
+  "/home/nova/Repository/freeture/src/Mask.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Mask.cpp.o"
+  "/home/nova/Repository/freeture/src/OpenSSL.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/OpenSSL.cpp.o"
+  "/home/nova/Repository/freeture/src/PixFmtConv.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/PixFmtConv.cpp.o"
+  "/home/nova/Repository/freeture/src/SMTPClient.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/SMTPClient.cpp.o"
+  "/home/nova/Repository/freeture/src/SaveImg.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/SaveImg.cpp.o"
+  "/home/nova/Repository/freeture/src/Stack.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/Stack.cpp.o"
+  "/home/nova/Repository/freeture/src/StackThread.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/StackThread.cpp.o"
+  "/home/nova/Repository/freeture/src/TimeDate.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/TimeDate.cpp.o"
+  "/home/nova/Repository/freeture/src/main.cpp" "/home/nova/Repository/freeture/cmake/CMakeFiles/FreeTure.dir/home/nova/Repository/freeture/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_CHRONO_DYN_LINK"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_LOG_DYN_LINK"
+  "BOOST_LOG_SETUP_DYN_LINK"
+  "BOOST_PROGRAM_OPTIONS_DYN_LINK"
+  "BOOST_REGEX_DYN_LINK"
+  "BOOST_SERIALIZATION_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/aravis-0.8"
+  "."
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
