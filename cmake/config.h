47fec111f42ce06186ea6b584fc0ff3b4e61075e
Mattia Malacarne <malacarne.mattia97@gmail.com>
2023-04-17 11:39:13 +0200

    Edit gitignore for config

---
+++ cmake/config.h
@@ -0,0 +1,10 @@
+#ifndef FREETURE_CONFIG_H
+#define FREETURE_CONFIG_H
+
+#define VERSION "1.3.337"
+#define CFG_PATH "/usr/local/share/freeture/"
+/* #undef WINDOWS */
+#define LINUX
+/* #undef USE_PYLON */
+
+#endif // FREETURE_CONFIG_H
