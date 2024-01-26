00c47cf0982279f1f4f49ea5f0467204642fd6ac
Mattia Malacarne <mattia.malacarne@n-3.it>
2024-01-26 11:16:18 +0100

    Fix data path

--- src/CameraFirstInit.h
+++ src/CameraFirstInit.h
@@ -1,2 +1,2 @@
-#define FREETURE_WRITE_CONFIG_PATH "/freeture/config_new.cfg"
-#define FREETURE_CHECK_INIT_DONE "/freeture/initdone"
+#define FREETURE_WRITE_CONFIG_PATH "/usr/local/share/freeture/config_new.cfg"
+#define FREETURE_CHECK_INIT_DONE "/usr/local/share/freeture/initdone"
